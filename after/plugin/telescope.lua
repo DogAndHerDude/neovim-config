f39efc100c9cc417dd65d2561c924677396286cb
Dominykas Bartkus <dominykasbart@gmail.com>
2023-05-08 10:11:32 +0300

    Literally no idea what changed

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,25 @@
+require('telescope').setup {
+  defaults = {
+    file_ignore_patterns = {"node_modules"}
+  }
+}
+
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>ps', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") });
+  builtin.grep_string({ search = vim.fn.input("grep > ") });
 end)
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer' })

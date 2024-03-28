2010d06a54859002dd8d0ee8a05911111ed03e9d
Dominykas Bartkus <dominykasbart@gmail.com>
2024-03-28 09:39:10 +0200

    chore(lsp): fix mappings for find backwards

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -127,7 +127,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end, opts)
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '<space>r', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', 'F', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<space>f', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     -- vim.keymap.set('n', '<space>f', function()
     --   vim.lsp.buf.format { async = true }

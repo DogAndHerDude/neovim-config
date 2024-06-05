9dab52ae723134ad698ea050dd9095f823c8b110
Dominykas Bartkus <dominykasbart@gmail.com>
2024-06-05 23:54:44 +0300

    add the new command as a keymap for convenience

--- lua/dominykas/remap.lua
+++ lua/dominykas/remap.lua
@@ -1,6 +1,10 @@
+local my_commands = require("dominykas.commands")
+
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- custom commands
+vim.keymap.set("n", "<leader>jq", function() my_commands.jsq() end)

49de94fb3c2179ae00617c46f12d65407440c17a
Dominykas Bartkus <dominykasbart@gmail.com>
2024-06-06 00:07:31 +0300

    clean up the function

--- lua/dominykas/commands.lua
+++ lua/dominykas/commands.lua
@@ -1,47 +1,34 @@
+local replacements = {
+  ['\\"'] = '"',
+  ['"{'] = '{',
+  ['}"'] = '}'
+}
+
 local function jsq_command()
   local file_buffer = vim.api.nvim_get_current_buf()
   local possible_json = vim.api.nvim_buf_get_lines(file_buffer, 0, -1, false)
-  local len = #possible_json
+  local str = table.concat(possible_json, "\n")
 
-  if len == 0 then
-    vim.api.nvim_cmd('echo "Buffer is empty"', {})
+  if #str == 0 then
+    print("Buffer is empty")
     return
   end
 
-  if len == 1 and string.match(possible_json[1], "^%{.*%}$") == nil then
-    vim.api.nvim_cmd('echo "Buffer malformed"', {})
-    return
-  end
-
-  if len > 1 and (string.match(possible_json[1], "^%{") == nil or string.match(possible_json[len], ".*%}$")) == nil then
-    vim.api.nvim_cmd('echo "Buffer malformed"', {})
+  if string.match(str, "^%{.*%}$") == nil then
+    print("Buffer malformed")
     return
   end
 
   vim.bo.filetype = "json"
 
-  if len == 1 then
-    possible_json[1] = string.gsub(string.gsub(possible_json[1], "^%{", "["), "%}$", "]")
-  end
+  str = string.gsub(string.gsub(str, "^%{", "["), "%}$", "]")
 
-  if len > 1 then
-    possible_json[1] = string.gsub(possible_json[1], "^%{", "[")
-    possible_json[len] = string.gsub(possible_json[len], "%}$", "]")
-  end
-
-  local replacements = {
-    ['\\"'] = '"',
-    ['"{'] = '{',
-    ['}"'] = '}'
-  }
-
-  local concated = table.concat(possible_json, "\n")
 
   for pattern, replacement in pairs(replacements) do
-    concated = string.gsub(concated, pattern, replacement)
+    str = string.gsub(str, pattern, replacement)
   end
 
-  local output = vim.fn.system("jq .", concated)
+  local output = vim.fn.system("jq .", str)
 
   vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(output, "\n"))
 end

153e68893d88c06bbd1c8a3b9d04221fa5e37e5a
Dominykas Bartkus <dominykas@walk15.app>
2026-04-09 11:07:07 +0300

    ugrade neovim & use its preferred package manager

---
+++ lua/dominykas/plugins.lua
@@ -0,0 +1,68 @@
+-- Dependencies must be listed before their dependents
+vim.pack.add({
+  { src = 'https://github.com/nvim-lua/plenary.nvim' },
+  { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+  { src = 'https://github.com/folke/snacks.nvim' },
+
+  { src = 'https://github.com/nvim-telescope/telescope.nvim',  version = '0.1.8' },
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
+  { src = 'https://github.com/neanias/everforest-nvim' },
+  { src = 'https://github.com/L3MON4D3/LuaSnip' },
+  { src = 'https://github.com/hrsh7th/nvim-cmp' },
+  { src = 'https://github.com/saadparwaiz1/cmp_luasnip' },
+  { src = 'https://github.com/hrsh7th/cmp-nvim-lsp' },
+  { src = 'https://github.com/hrsh7th/cmp-buffer' },
+  { src = 'https://github.com/hrsh7th/cmp-path' },
+  { src = 'https://github.com/rafamadriz/friendly-snippets' },
+  { src = 'https://github.com/hrsh7th/cmp-nvim-lua' },
+  { src = 'https://github.com/nvim-lualine/lualine.nvim' },
+  { src = 'https://github.com/MunifTanjim/prettier.nvim' },
+  { src = 'https://github.com/williamboman/mason.nvim' },
+  { src = 'https://github.com/onsails/lspkind.nvim' },
+  { src = 'https://github.com/akinsho/bufferline.nvim' },
+  { src = 'https://github.com/tpope/vim-fugitive' },
+  { src = 'https://github.com/dinhhuy258/git.nvim' },
+  { src = 'https://github.com/windwp/nvim-ts-autotag' },
+  { src = 'https://github.com/lewis6991/gitsigns.nvim' },
+  { src = 'https://github.com/stevearc/conform.nvim' },
+  { src = 'https://github.com/xemptuous/sqlua.nvim' },
+  { src = 'https://github.com/devswiftzone/swift.nvim' },
+  { src = 'https://github.com/coder/claudecode.nvim' },
+})
+
+require('everforest').setup({
+  background = 'hard',
+  transparent_background_level = 0,
+})
+vim.cmd('colorscheme everforest')
+
+require('nvim-ts-autotag').setup()
+
+local swift_lsp = require('swift.features.lsp')
+swift_lsp.lsp_setup_done = true
+
+require('swift').setup({
+  format_on_save = true,
+  tool = 'swift-format',
+  lsp = { enabled = false },
+})
+
+require('claudecode').setup()
+
+vim.keymap.set('n', '<leader>ac', '<cmd>ClaudeCode<cr>', { desc = 'Toggle Claude' })
+vim.keymap.set('n', '<leader>af', '<cmd>ClaudeCodeFocus<cr>', { desc = 'Focus Claude' })
+vim.keymap.set('n', '<leader>ar', '<cmd>ClaudeCode --resume<cr>', { desc = 'Resume Claude' })
+vim.keymap.set('n', '<leader>aC', '<cmd>ClaudeCode --continue<cr>', { desc = 'Continue Claude' })
+vim.keymap.set('n', '<leader>am', '<cmd>ClaudeCodeSelectModel<cr>', { desc = 'Select Claude model' })
+vim.keymap.set('n', '<leader>ab', '<cmd>ClaudeCodeAdd %<cr>', { desc = 'Add current buffer' })
+vim.keymap.set('v', '<leader>as', '<cmd>ClaudeCodeSend<cr>', { desc = 'Send to Claude' })
+vim.keymap.set('n', '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>', { desc = 'Accept diff' })
+vim.keymap.set('n', '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', { desc = 'Deny diff' })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'NvimTree', 'neo-tree', 'oil', 'minifiles', 'netrw' },
+  callback = function(ev)
+    vim.keymap.set('n', '<leader>as', '<cmd>ClaudeCodeTreeAdd<cr>',
+      { desc = 'Add file', buffer = ev.buf })
+  end,
+})

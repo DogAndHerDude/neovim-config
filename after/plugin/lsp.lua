f4e276bc376c52114f8d000e0ff88d645e96844d
Dominykas Bartkus <dominykas.b@l3whjnh94k.localdomain>
2026-05-27 11:23:09 +0300

    add lua lsp & format the hell out of it

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,71 +1,71 @@
-local cmp = require('cmp')
-local capabilities = require('cmp_nvim_lsp')
+local cmp = require("cmp")
+local capabilities = require("cmp_nvim_lsp")
 
 local default_capabilities = capabilities.default_capabilities()
 
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      require 'luasnip'.lsp_expand(args.body)
-    end
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'nvim_lua' },
-    { name = 'luasnip' },
-    { name = 'path' },
-  }, {
-    { name = 'buffer' },
-  }),
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  }),
-}
-
-vim.lsp.config('eslint', {
-  on_attach = function(client, bufnr)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = bufnr,
-      callback = function()
-        -- This is the code that 'EslintFixAll' executes under the hood
-        vim.lsp.buf.execute_command({
-          command = 'eslint.applyAllFixes',
-          arguments = {
-            {
-              uri = vim.uri_from_bufnr(bufnr),
-              version = vim.lsp.util.buf_versions[bufnr],
-            },
-          },
-        })
-      end,
-    })
-  end,
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+		{ name = "luasnip" },
+		{ name = "path" },
+	}, {
+		{ name = "buffer" },
+	}),
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	}),
 })
-vim.lsp.enable('eslint')
 
-vim.lsp.config('gopls', {
-  capabilities = default_capabilities,
-  settings = {
-    gopls = {
-      staticcheck = true,
-      --gofumpt = true,
-      env = {
-        GOFUMPT_SPLIT_LONG_LINES = "on",
-      },
-    }
-  }
+vim.lsp.config("eslint", {
+	on_attach = function(client, bufnr)
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			buffer = bufnr,
+			callback = function()
+				-- This is the code that 'EslintFixAll' executes under the hood
+				vim.lsp.buf.execute_command({
+					command = "eslint.applyAllFixes",
+					arguments = {
+						{
+							uri = vim.uri_from_bufnr(bufnr),
+							version = vim.lsp.util.buf_versions[bufnr],
+						},
+					},
+				})
+			end,
+		})
+	end,
 })
-vim.lsp.enable('gopls')
+vim.lsp.enable("eslint")
 
-vim.lsp.config('ts_ls', {})
+vim.lsp.config("gopls", {
+	capabilities = default_capabilities,
+	settings = {
+		gopls = {
+			staticcheck = true,
+			--gofumpt = true,
+			--env = {
+			--  GOFUMPT_SPLIT_LONG_LINES = "on",
+			--},
+		},
+	},
+})
+vim.lsp.enable("gopls")
+
+vim.lsp.config("ts_ls", {})
 --vim.api.nvim_create_autocmd('LspAttach', {
 --  callback = function(args)
 --    local client = vim.lsp.get_client_by_id(args.data.client_id)
@@ -80,149 +80,149 @@ vim.lsp.config('ts_ls', {})
 --    vim.bo.syntax = ''
 --  end,
 --})
-vim.lsp.enable('ts_ls')
+vim.lsp.enable("ts_ls")
 
-vim.lsp.config('graphql', {
-  capabilities = default_capabilities,
+vim.lsp.config("graphql", {
+	capabilities = default_capabilities,
 })
-vim.lsp.enable('graphql')
-vim.lsp.config('prismals', {
-  capabilities = default_capabilities,
+vim.lsp.enable("graphql")
+vim.lsp.config("prismals", {
+	capabilities = default_capabilities,
 })
-vim.lsp.enable('prismals')
-vim.lsp.enable('tailwindcss')
-vim.lsp.enable('astro')
-vim.lsp.config('rust_analyzer', {
-  capabilities = default_capabilities,
-  -- Server-specific settings. See `:help vim.lsp.config-setup`
-  settings = {
-    ['rust-analyzer'] = {},
-  },
+vim.lsp.enable("prismals")
+vim.lsp.enable("tailwindcss")
+vim.lsp.enable("astro")
+vim.lsp.config("rust_analyzer", {
+	capabilities = default_capabilities,
+	-- Server-specific settings. See `:help vim.lsp.config-setup`
+	settings = {
+		["rust-analyzer"] = {},
+	},
 })
-vim.lsp.enable('rust_analyzer')
-vim.lsp.config('lua_ls', {
-  capabilities = default_capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = {
-          'vim',
-          'require'
-        },
-      },
-    },
-  }
+vim.lsp.enable("rust_analyzer")
+vim.lsp.config("lua_ls", {
+	capabilities = default_capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = {
+					"vim",
+					"require",
+				},
+			},
+		},
+	},
 })
-vim.lsp.enable('lua_ls')
+vim.lsp.enable("lua_ls")
 
-vim.lsp.config('html', {
-  capabilities = default_capabilities,
+vim.lsp.config("html", {
+	capabilities = default_capabilities,
 })
-vim.lsp.enable('html')
+vim.lsp.enable("html")
 --It's dead, Jimbo...
 --vim.lsp.config.htmx.setup {
 --  capabilities = capabilities,
 --}
 
-vim.lsp.config('templ', {
-  capabilities = default_capabilities,
-  filetypes = { "templ" }
+vim.lsp.config("templ", {
+	capabilities = default_capabilities,
+	filetypes = { "templ" },
 })
-vim.lsp.enable('templ')
+vim.lsp.enable("templ")
 
-vim.lsp.config('ansible-lint', {
-  capabilities = default_capabilities,
-  ansible = {
-    ansible = {
-      path = "ansible"
-    },
-    executionEnvironment = {
-      enabled = false
-    },
-    python = {
-      interpreterPath = "python"
-    },
-    validation = {
-      enabled = true,
-      lint = {
-        enabled = true,
-        path = "ansible-lint"
-      }
-    }
-  }
+vim.lsp.config("ansible-lint", {
+	capabilities = default_capabilities,
+	ansible = {
+		ansible = {
+			path = "ansible",
+		},
+		executionEnvironment = {
+			enabled = false,
+		},
+		python = {
+			interpreterPath = "python",
+		},
+		validation = {
+			enabled = true,
+			lint = {
+				enabled = true,
+				path = "ansible-lint",
+			},
+		},
+	},
 })
-vim.lsp.enable('ansible-lint')
+vim.lsp.enable("ansible-lint")
 
-vim.lsp.enable('ansiblels')
+vim.lsp.enable("ansiblels")
 
-vim.lsp.config('sourcekit', {
-  cmd = { "xcrun", "sourcekit-lsp" },
+vim.lsp.config("sourcekit", {
+	cmd = { "xcrun", "sourcekit-lsp" },
 
-  filetypes = { "swift", "c", "cpp", "objective-c", "objective-cpp" },
+	filetypes = { "swift", "c", "cpp", "objective-c", "objective-cpp" },
 
-  root_markers = { "buildServer.json", "*.xcodeproj", ".git" },
+	root_markers = { "buildServer.json", "*.xcodeproj", ".git" },
 
-  capabilities = default_capabilities,
-  --capabilities = {
-  --  workspace = {
-  --    didChangeWatchedFiles = {
-  --      dynamicRegistration = true,
-  --    },
-  --  },
-  --},
+	capabilities = default_capabilities,
+	--capabilities = {
+	--  workspace = {
+	--    didChangeWatchedFiles = {
+	--      dynamicRegistration = true,
+	--    },
+	--  },
+	--},
 
-  on_attach = function(client, bufnr)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { noremap = true, silent = true })
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true })
-  end,
+	on_attach = function(client, bufnr)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true })
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })
+	end,
 })
-vim.lsp.enable('sourcekit')
+vim.lsp.enable("sourcekit")
 
-vim.lsp.config('zls', {
-  cmd = { "zls" },
-  filetypes = { "zig", "zir" },
-  root_markers = { "build.zig", ".git" },
-  single_file_support = true,
-  capabilities = default_capabilities,
-  settings = {
-    zls = {
-      zig_exe_path = "/opt/homebrew/bin/zig",
-      warn_style = true,
-      highlight_global_var_declarations = true,
-      build_on_save = true,
-    }
-  },
+vim.lsp.config("zls", {
+	cmd = { "zls" },
+	filetypes = { "zig", "zir" },
+	root_markers = { "build.zig", ".git" },
+	single_file_support = true,
+	capabilities = default_capabilities,
+	settings = {
+		zls = {
+			zig_exe_path = "/opt/homebrew/bin/zig",
+			warn_style = true,
+			highlight_global_var_declarations = true,
+			build_on_save = true,
+		},
+	},
 })
-vim.lsp.enable('zls')
+vim.lsp.enable("zls")
 
-vim.lsp.config('kotlin_language_server', {
-  cmd = { "kotlin-language-server" },
-  filetypes = { "kotlin" },
-  root_markers = {
-    "settings.gradle",
-    "settings.gradle.kts",
-    "build.xml",
-    "pom.xml",
-    "build.gradle",
-    "build.gradle.kts"
-  },
-  init_options = {
-    storagePath = vim.fn.stdpath("data") .. "/kotlin-language-server",
-  },
-  settings = {
-    kotlin = {
-      compiler = {
-        jvm = {
-          target = "default"
-        }
-      },
-      linting = {
-        debounceTime = 500
-      }
-    }
-  },
+vim.lsp.config("kotlin_language_server", {
+	cmd = { "kotlin-language-server" },
+	filetypes = { "kotlin" },
+	root_markers = {
+		"settings.gradle",
+		"settings.gradle.kts",
+		"build.xml",
+		"pom.xml",
+		"build.gradle",
+		"build.gradle.kts",
+	},
+	init_options = {
+		storagePath = vim.fn.stdpath("data") .. "/kotlin-language-server",
+	},
+	settings = {
+		kotlin = {
+			compiler = {
+				jvm = {
+					target = "default",
+				},
+			},
+			linting = {
+				debounceTime = 500,
+			},
+		},
+	},
 })
-vim.lsp.enable('kotlin_language_server')
+vim.lsp.enable("kotlin_language_server")
 
 -- it borken
 --vim.lsp.config.sqls.setup {
@@ -231,41 +231,40 @@ vim.lsp.enable('kotlin_language_server')
 --  end
 --}
 
-
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>r', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<space>f', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    -- vim.keymap.set('n', '<space>f', function()
-    --   vim.lsp.buf.format { async = true }
-    -- end, opts)
-  end,
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+		vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set("n", "<space>wl", function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<space>r", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "<space>f", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		-- vim.keymap.set('n', '<space>f', function()
+		--   vim.lsp.buf.format { async = true }
+		-- end, opts)
+	end,
 })
--- lua/dominykas/plugins.lua
+++ lua/dominykas/plugins.lua
@@ -1,44 +1,45 @@
 -- Dependencies must be listed before their dependents
 vim.pack.add({
-  { src = 'https://github.com/nvim-lua/plenary.nvim' },
-  { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
-  { src = 'https://github.com/folke/snacks.nvim' },
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	{ src = "https://github.com/folke/snacks.nvim" },
 
-  { src = 'https://github.com/nvim-telescope/telescope.nvim' },
-  {
-    src = 'https://github.com/nvim-treesitter/nvim-treesitter',
-    version = 'main',
-  },
-  { src = 'https://github.com/neanias/everforest-nvim' },
-  { src = 'https://github.com/rebelot/kanagawa.nvim' },
-  { src = 'https://github.com/L3MON4D3/LuaSnip' },
-  { src = 'https://github.com/hrsh7th/nvim-cmp' },
-  { src = 'https://github.com/saadparwaiz1/cmp_luasnip' },
-  { src = 'https://github.com/hrsh7th/cmp-nvim-lsp' },
-  { src = 'https://github.com/hrsh7th/cmp-buffer' },
-  { src = 'https://github.com/hrsh7th/cmp-path' },
-  { src = 'https://github.com/rafamadriz/friendly-snippets' },
-  { src = 'https://github.com/hrsh7th/cmp-nvim-lua' },
-  { src = 'https://github.com/nvim-lualine/lualine.nvim' },
-  { src = 'https://github.com/MunifTanjim/prettier.nvim' },
-  { src = 'https://github.com/williamboman/mason.nvim' },
-  { src = 'https://github.com/onsails/lspkind.nvim' },
-  { src = 'https://github.com/akinsho/bufferline.nvim' },
-  { src = 'https://github.com/tpope/vim-fugitive' },
-  { src = 'https://github.com/dinhhuy258/git.nvim' },
-  { src = 'https://github.com/windwp/nvim-ts-autotag' },
-  { src = 'https://github.com/lewis6991/gitsigns.nvim' },
-  { src = 'https://github.com/stevearc/conform.nvim' },
-  { src = 'https://github.com/xemptuous/sqlua.nvim' },
-  { src = 'https://github.com/devswiftzone/swift.nvim' },
-  { src = 'https://github.com/coder/claudecode.nvim' },
+	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
+	{
+		src = "https://github.com/nvim-treesitter/nvim-treesitter",
+		version = "main",
+	},
+	{ src = "https://github.com/neanias/everforest-nvim" },
+	{ src = "https://github.com/rebelot/kanagawa.nvim" },
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },
+	{ src = "https://github.com/hrsh7th/nvim-cmp" },
+	{ src = "https://github.com/saadparwaiz1/cmp_luasnip" },
+	{ src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
+	{ src = "https://github.com/hrsh7th/cmp-buffer" },
+	{ src = "https://github.com/hrsh7th/cmp-path" },
+	{ src = "https://github.com/rafamadriz/friendly-snippets" },
+	{ src = "https://github.com/hrsh7th/cmp-nvim-lua" },
+	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
+	{ src = "https://github.com/MunifTanjim/prettier.nvim" },
+	{ src = "https://github.com/williamboman/mason.nvim" },
+	{ src = "https://github.com/onsails/lspkind.nvim" },
+	{ src = "https://github.com/akinsho/bufferline.nvim" },
+	{ src = "https://github.com/tpope/vim-fugitive" },
+	{ src = "https://github.com/dinhhuy258/git.nvim" },
+	{ src = "https://github.com/windwp/nvim-ts-autotag" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+	{ src = "https://github.com/stevearc/conform.nvim" },
+	{ src = "https://github.com/xemptuous/sqlua.nvim" },
+	{ src = "https://github.com/devswiftzone/swift.nvim" },
+	{ src = "https://github.com/coder/claudecode.nvim" },
+	{ src = "https://github.com/tpope/vim-fugitive" },
 })
 
-require('everforest').setup({
-  background = 'hard',
-  transparent_background_level = 0,
+require("everforest").setup({
+	background = "hard",
+	transparent_background_level = 0,
 })
-vim.cmd('colorscheme everforest')
+vim.cmd("colorscheme everforest")
 -- Annoying typescript methods not being highlighted... Thanks treesitter & ts_ls...
 --vim.api.nvim_create_autocmd('ColorScheme', {
 --  callback = function()
@@ -55,41 +56,70 @@ vim.cmd('colorscheme everforest')
 --require('kanagawa').setup({})
 --vim.cmd('colorscheme kanagawa')
 
-local treesitter = require('nvim-treesitter')
+local treesitter = require("nvim-treesitter")
 
 treesitter.setup({
-  install_dir = vim.fn.stdpath('data') .. '/site'
+	install_dir = vim.fn.stdpath("data") .. "/site",
 })
 
-treesitter.install {
-  "astro", "javascript", "typescript", "tsx", "c", "lua", "vim", "query",
-  "prisma", "rust", "scss", "graphql", "css", "cpp", "proto", "go",
-  "gomod", "gowork", "gotmpl", "gosum", "sql", "templ", "html",
-  "make", "swift", "vimdoc", "zig", "markdown", "markdown_inline", "json",
-  "xml", "kotlin", "bash", "php"
-}
+treesitter.install({
+	"astro",
+	"javascript",
+	"typescript",
+	"tsx",
+	"c",
+	"lua",
+	"vim",
+	"query",
+	"prisma",
+	"rust",
+	"scss",
+	"graphql",
+	"css",
+	"cpp",
+	"proto",
+	"go",
+	"gomod",
+	"gowork",
+	"gotmpl",
+	"gosum",
+	"sql",
+	"templ",
+	"html",
+	"make",
+	"swift",
+	"vimdoc",
+	"zig",
+	"markdown",
+	"markdown_inline",
+	"json",
+	"xml",
+	"kotlin",
+	"bash",
+	"php",
+})
 
 -- Enable treesitter highlighting
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function()
-    pcall(vim.treesitter.start)
-  end,
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function()
+		pcall(vim.treesitter.start)
+	end,
 })
 
 -- Enable treesitter indentation
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function()
-    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-  end,
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function()
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+	end,
 })
 
 -- Enable treesitter folding
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function()
-    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-    vim.wo[0][0].foldmethod = 'expr'
-    vim.wo[0][0].foldlevel = 99 -- start with all folds open
-  end,
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function()
+		vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		vim.wo[0][0].foldmethod = "expr"
+		vim.wo[0][0].foldlevel = 99 -- start with all folds open
+	end,
 })
 
 --treesitter.setup {
@@ -104,35 +134,34 @@ vim.api.nvim_create_autocmd('FileType', {
 --  },
 --}
 
-require('nvim-ts-autotag').setup()
+require("nvim-ts-autotag").setup()
 
-local swift_lsp = require('swift.features.lsp')
+local swift_lsp = require("swift.features.lsp")
 swift_lsp.lsp_setup_done = true
 
-require('swift').setup({
-  format_on_save = true,
-  tool = 'swift-format',
-  lsp = { enabled = false },
+require("swift").setup({
+	format_on_save = true,
+	tool = "swift-format",
+	lsp = { enabled = false },
 })
 
-require('claudecode').setup()
+require("claudecode").setup()
 
-vim.keymap.set('n', '<leader>ac', '<cmd>ClaudeCode<cr>', { desc = 'Toggle Claude' })
-vim.keymap.set('n', '<leader>af', '<cmd>ClaudeCodeFocus<cr>', { desc = 'Focus Claude' })
-vim.keymap.set('n', '<leader>ar', '<cmd>ClaudeCode --resume<cr>', { desc = 'Resume Claude' })
-vim.keymap.set('n', '<leader>aC', '<cmd>ClaudeCode --continue<cr>', { desc = 'Continue Claude' })
-vim.keymap.set('n', '<leader>am', '<cmd>ClaudeCodeSelectModel<cr>', { desc = 'Select Claude model' })
-vim.keymap.set('n', '<leader>ab', '<cmd>ClaudeCodeAdd %<cr>', { desc = 'Add current buffer' })
-vim.keymap.set('v', '<leader>as', '<cmd>ClaudeCodeSend<cr>', { desc = 'Send to Claude' })
-vim.keymap.set('n', '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>', { desc = 'Accept diff' })
-vim.keymap.set('n', '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', { desc = 'Deny diff' })
+vim.keymap.set("n", "<leader>ac", "<cmd>ClaudeCode<cr>", { desc = "Toggle Claude" })
+vim.keymap.set("n", "<leader>af", "<cmd>ClaudeCodeFocus<cr>", { desc = "Focus Claude" })
+vim.keymap.set("n", "<leader>ar", "<cmd>ClaudeCode --resume<cr>", { desc = "Resume Claude" })
+vim.keymap.set("n", "<leader>aC", "<cmd>ClaudeCode --continue<cr>", { desc = "Continue Claude" })
+vim.keymap.set("n", "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", { desc = "Select Claude model" })
+vim.keymap.set("n", "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", { desc = "Add current buffer" })
+vim.keymap.set("v", "<leader>as", "<cmd>ClaudeCodeSend<cr>", { desc = "Send to Claude" })
+vim.keymap.set("n", "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", { desc = "Accept diff" })
+vim.keymap.set("n", "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", { desc = "Deny diff" })
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'NvimTree', 'neo-tree', 'oil', 'minifiles', 'netrw' },
-  callback = function(ev)
-    vim.keymap.set('n', '<leader>as', '<cmd>ClaudeCodeTreeAdd<cr>',
-      { desc = 'Add file', buffer = ev.buf })
-  end,
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" },
+	callback = function(ev)
+		vim.keymap.set("n", "<leader>as", "<cmd>ClaudeCodeTreeAdd<cr>", { desc = "Add file", buffer = ev.buf })
+	end,
 })
 
 vim.opt.hlsearch = true
